52f7d94e1de50478d18409a16875da056ac5356c
larspeelman <lars.peelman@hotmail.com>
2018-12-10 11:16:03 +0100

    getall campaigns front

--- switchfully-intakeapp-frontend/src/app/core/apiUrl/apiUrl.ts
+++ switchfully-intakeapp-frontend/src/app/core/apiUrl/apiUrl.ts
@@ -1,3 +1,4 @@
 export class ApiUrl{
     static readonly urlUsers = 'http://localhost:59089/api/Users/';
+    static readonly urlCampaign = 'http://localhost:59089/api/Campaigns/';
 }
--- switchfully-intakeapp-frontend/src/app/core/campaigns/campaign.service.ts
+++ switchfully-intakeapp-frontend/src/app/core/campaigns/campaign.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Campaign } from './campaign';
 import { Observable } from 'rxjs';
+import { ApiUrl } from '../apiUrl/apiUrl';
 
 @Injectable()
 export class CampaignService {
-  private campaignsUrl = 'http://localhost:59089/api/Campaigns'
+
   
   constructor(private http: HttpClient) { }
   
   getCampaigns(): Observable<Campaign[]> {
-    return this.http.get<Campaign[]>(this.campaignsUrl);
+    return this.http.get<Campaign[]>(ApiUrl.urlCampaign);
   }
 
 }
